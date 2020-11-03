39090d2050c844026bff7667972f3cadcd7b16f9
Isaac Debiasi <isaacdbs@outlook.com>
2020-11-03 17:34:19 -0300

    Adicionada secao 13 - Trabalhando com arquivos

---
+++ Secao_13_Arquivos/Exercicio1/Exercicio1/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Exercicio1.Entities
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public decimal Value { get; set; }
+        public int Quantity { get; set; }
+
+        public Product(string name, decimal value, int quantity)
+        {
+            Name = name;
+            Value = value;
+            Quantity = quantity;
+        }
+
+        public decimal SubTotal()
+        {
+            return Value * Quantity;
+        }
+
+        public override string ToString()
+        {
+            return" "+ Name + ", $" + SubTotal().ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Secao_13_Arquivos/Exercicio1/Exercicio1/Program.cs
@@ -0,0 +1,44 @@
+﻿using Exercicio1.Entities;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+
+namespace Exercicio1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\CursoCSharp\Curso_CShrap\Secao_13_Arquivos\Repository";
+
+            try
+            {
+
+                string[] lines = File.ReadAllLines(path + @"\file.csv");
+
+                Directory.CreateDirectory(path + @"\out");
+
+                using (StreamWriter sw = File.AppendText(path + @"\out\summary.csv"))
+                {
+                    for (int i = 0; i < lines.Length; i++)
+                    {
+                        string[] itemData = lines[i].Split(',');
+
+                        string name = itemData[0];
+                        decimal value = decimal.Parse(itemData[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(itemData[2]);
+
+                        Product prod = new Product(name, value, quantity);
+
+                        sw.WriteLine(prod);
+                    }
+                }
+            }
+            catch(IOException e)
+            {
+                Console.WriteLine("An Error ocurred: " + e.Message);
+            }
+        }
+    }
+}
